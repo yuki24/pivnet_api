685a3bc56f09eb398bc29042999d5de18ee21c94
Yuki Nishijima <mail@yukinishijima.net>
2016-10-13 04:34:04 +0000

    Remove trailing whitespaces

--- lib/pivnet_api/client.rb
+++ lib/pivnet_api/client.rb
@@ -19,207 +19,207 @@ class PivnetAPI::Client
   def register_observer(observer)
     @observers << observer
   end
-  
+
   def authentication(query_params: {}, headers: {}, **options)
     request(Net::HTTP::Get, uri("/api/v2/authentication", query_params), nil, headers, options)
   end
   
-  
+
   def get_products(query_params: {}, headers: {}, **options)
     request(Net::HTTP::Get, uri("/api/v2/products", query_params), nil, headers, options)
   end
   alias find_products get_products
-  
+
   def get_product(slug, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Get, uri("/api/v2/products/#{slug}", query_params), nil, headers, options)
   end
   alias find_product get_product
-  
+
   def sort_release_for_product(slug, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Patch, uri("/api/v2/products/#{slug}/release_sort_order", query_params), body, headers, options)
   end
   
-  
+
   def get_eulas(query_params: {}, headers: {}, **options)
     request(Net::HTTP::Get, uri("/api/v2/eulas", query_params), nil, headers, options)
   end
   alias find_eulas get_eulas
-  
+
   def get_eula(id, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Get, uri("/api/v2/eulas/#{id}", query_params), nil, headers, options)
   end
   alias find_eula get_eula
-  
+
   def accept_eula(slug, release_id, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Post, uri("/api/v2/products/#{slug}/releases/#{release_id}/eula_acceptance", query_params), body, headers, options)
   end
   
-  
+
   def get_user_groups(query_params: {}, headers: {}, **options)
     request(Net::HTTP::Get, uri("/api/v2/user_groups", query_params), nil, headers, options)
   end
   alias find_user_groups get_user_groups
-  
+
   def get_user_group(id, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Get, uri("/api/v2/user_groups/#{id}", query_params), nil, headers, options)
   end
   alias find_user_group get_user_group
-  
+
   def post_user_group(body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Post, uri("/api/v2/user_groups", query_params), body, headers, options)
   end
   alias create_user_group post_user_group
-  
+
   def patch_user_group(id, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Patch, uri("/api/v2/user_groups/#{id}", query_params), body, headers, options)
   end
   alias update_user_group patch_user_group
-  
+
   def delete_user_group(id, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Delete, uri("/api/v2/user_groups/#{id}", query_params), nil, headers, options)
   end
   alias destroy_user_group delete_user_group
-  
+
   def add_member_to_group(id, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Patch, uri("/api/v2/user_groups/#{id}/add_member", query_params), body, headers, options)
   end
   
-  
+
   def remove_member_from_group(id, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Patch, uri("/api/v2/user_groups/#{id}/remove_member", query_params), body, headers, options)
   end
   
-  
+
   def get_product_files(slug, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Get, uri("/api/v2/products/#{slug}/product_files", query_params), nil, headers, options)
   end
   alias find_product_files get_product_files
-  
+
   def get_product_file(slug, id, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Get, uri("/api/v2/products/#{slug}/product_files/#{id}", query_params), nil, headers, options)
   end
   alias find_product_file get_product_file
-  
+
   def post_product_file(slug, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Post, uri("/api/v2/products/#{slug}/product_files", query_params), body, headers, options)
   end
   alias create_product_file post_product_file
-  
+
   def patch_product_file(slug, id, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Patch, uri("/api/v2/products/#{slug}/product_files/#{id}", query_params), body, headers, options)
   end
   alias update_product_file patch_product_file
-  
+
   def delete_product_file(slug, id, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Delete, uri("/api/v2/products/#{slug}/product_files/#{id}", query_params), nil, headers, options)
   end
   alias destroy_product_file delete_product_file
-  
+
   def retry_file_transfer(slug, id, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Patch, uri("/api/v2/products/#{slug}/product_files/retry_all", query_params), body, headers, options)
   end
   
-  
+
   def post_download(slug, release_id, id, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Post, uri("/api/v2/products/#{slug}/releases/#{release_id}/product_files/#{id}/download", query_params), body, headers, options)
   end
   alias create_download post_download
-  
+
   def post_signature_download(slug, release_id, id, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Post, uri("/api/v2/products/#{slug}/releases/#{release_id}/product_files/#{id}/signature_file_download", query_params), body, headers, options)
   end
   alias create_signature_download post_signature_download
-  
+
   def get_file_groups(slug, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Get, uri("/api/v2/products/#{slug}/file_groups", query_params), nil, headers, options)
   end
   alias find_file_groups get_file_groups
-  
+
   def get_file_group(slug, id, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Get, uri("/api/v2/products/#{slug}/file_groups/#{id}", query_params), nil, headers, options)
   end
   alias find_file_group get_file_group
-  
+
   def post_file_group(slug, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Post, uri("/api/v2/products/#{slug}/file_groups", query_params), body, headers, options)
   end
   alias create_file_group post_file_group
-  
+
   def patch_file_group(slug, id, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Patch, uri("/api/v2/products/#{slug}/file_groups/#{id}", query_params), body, headers, options)
   end
   alias update_file_group patch_file_group
-  
+
   def delete_file_group(slug, id, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Delete, uri("/api/v2/products/#{slug}/file_groups/#{id}", query_params), nil, headers, options)
   end
   alias destroy_file_group delete_file_group
-  
+
   def add_file_to_group(slug, id, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Patch, uri("/api/v2/products/#{slug}/file_groups/#{id}/add_product_file", query_params), body, headers, options)
   end
   
-  
+
   def remove_file_from_group(slug, id, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Delete, uri("/api/v2/products/#{slug}/file_groups/#{id}/remove_product_file", query_params), nil, headers, options)
   end
   
-  
+
   def sort_file_in_group(slug, id, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Patch, uri("/api/v2/products/#{slug}/file_groups/#{id}/product_file_sort_order", query_params), body, headers, options)
   end
   
-  
+
   def get_releases(slug, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Get, uri("/api/v2/products/#{slug}/releases", query_params), nil, headers, options)
   end
   alias find_releases get_releases
-  
+
   def get_release(slug, id, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Get, uri("/api/v2/products/#{slug}/releases/#{id}", query_params), nil, headers, options)
   end
   alias find_release get_release
-  
+
   def post_release(slug, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Post, uri("/api/v2/products/#{slug}/releases", query_params), body, headers, options)
   end
   alias create_release post_release
-  
+
   def patch_release(slug, id, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Patch, uri("/api/v2/products/#{slug}/releases/#{id}", query_params), body, headers, options)
   end
   alias update_release patch_release
-  
+
   def delete_release(slug, id, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Delete, uri("/api/v2/products/#{slug}/releases/#{id}", query_params), nil, headers, options)
   end
   alias destroy_release delete_release
-  
+
   def get_release_files(slug, release_id, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Get, uri("/api/v2/products/#{slug}/releases/#{release_id}/release_files", query_params), nil, headers, options)
   end
   alias find_release_files get_release_files
-  
+
   def get_release_file(slug, release_id, id, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Get, uri("/api/v2/products/#{slug}/releases/#{release_id}/release_files/#{id}", query_params), nil, headers, options)
   end
   alias find_release_file get_release_file
-  
+
   def post_release_file(slug, release_id, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Post, uri("/api/v2/products/#{slug}/releases/#{release_id}/release_files", query_params), body, headers, options)
   end
   alias create_release_file post_release_file
-  
+
   def patch_release_file(slug, release_id, id, body, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Patch, uri("/api/v2/products/#{slug}/releases/#{release_id}/release_files/#{id}", query_params), body, headers, options)
   end
   alias update_release_file patch_release_file
-  
+
   def delete_release_file(slug, release_id, id, query_params: {}, headers: {}, **options)
     request(Net::HTTP::Delete, uri("/api/v2/products/#{slug}/releases/#{release_id}/release_files/#{id}", query_params), nil, headers, options)
   end
   alias destroy_release_file delete_release_file
-  
+
   private
 
   DEFAULT_OPTIONS = {
